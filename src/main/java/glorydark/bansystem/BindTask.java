94eda8d297d5984fa51a62cadd8f51e53ab08965
Glorydark <darkteam@foxmail.com>
2022-08-09 00:24:54 +0800

    original upload

---
+++ src/main/java/glorydark/bansystem/BindTask.java
@@ -0,0 +1,29 @@
+package glorydark.bansystem;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.level.Position;
+import cn.nukkit.network.protocol.MovePlayerPacket;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BindTask implements Runnable {
+
+    public static ConcurrentHashMap<Player, Player> bindPlayers = new ConcurrentHashMap<>();
+
+    @Override
+    public void run() {
+        for(Player bind: bindPlayers.values()){
+            for (Player player : bindPlayers.keySet()) { //bind为被捆绑者
+                if (bindPlayers.get(player).equals(bind)) {
+                    if (bind.isOnline() && player.isOnline()) {
+                        player.sendPosition(new Position(bind.getX(),bind.getY(),bind.getZ()),bind.getYaw(),bind.getPitch(), MovePlayerPacket.MODE_NORMAL);
+                        player.sendTip("您正在跟随" + bind.getName());
+                    }else{
+                        bindPlayers.remove(player);
+                    }
+                }
+            }
+        }
+    }
+}
